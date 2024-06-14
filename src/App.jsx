8d5dc37af43e91513e93236fdb0385c010bdc8bb
D Sandhu <duvendersandhu@gmail.com>
2024-06-14 19:14:39 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState([])
+  useEffect(()=>{
+    let res= fetch('https://api.github.com/users').then(res=>res.json()).then(data=>setCount(data))
+  },[])
   return (
-    <h1>
+    <div>
+      <h1>
       Hello WOrld How areYOu
     </h1>
+    <ol>
+      {count.map(e=>{
+        return <li>{e.login}</li>
+      })}
+    </ol>
+    </div>
   )
 }
 
